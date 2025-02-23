c86d6f959734a052e7a408299072b9c18ed4c8b7
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2025-02-22 21:51:52 -0300

    :heavy_plus_sign: Adding "Firebase Auth" dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -59,6 +60,9 @@ dependencies {
 
     // Koin
     implementation(libs.bundles.koin.compose)
+
+    // Firebase Auth
+    implementation(libs.bundles.firebase.auth)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
